ea74e92d7de3af2bb2060ae3c18a48d8942250ba
abornaee <abornaee@users.noreply.github.com>
2024-09-18 21:18:37 +0330

    Update Layout.tsx

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -28,24 +28,7 @@ const Layout = ({ children, downloaders }: LayoutProps) => {
           downloaders={downloaders}
         />
       )}
-      <main>{children}</main>
-      {downloaders && <Footer downloaders={downloaders} />}
-      {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS && (
-        <>
-          <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-            strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-            page_path: window.location.pathname,
-          });
-        `}
+  
           </Script>
         </>
       )}
